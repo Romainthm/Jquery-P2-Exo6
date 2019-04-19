f7858aa693b821f9b8c7bf63b741fb0f9d4d0b09
romain <romain.thomine007@gmail.com>
2019-04-19 10:15:59 +0200

    finiA

---
+++ mains.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+      $('#green').mouseenter(function(){
+          $("#text").css("color","green");
+      });
+      $('#green').mouseleave(function(){
+          $("#text").css("color", "black");
+      });
+
+  });
+$(document).ready(function(){
+        $('#red').mouseenter(function(){
+            $("#text").css("color","red");
+        });
+        $('#red').mouseleave(function(){
+            $("#text").css("color", "black");
+        });
+
+    });
+$(document).ready(function(){
+      $('#blue').mouseenter(function(){
+          $("#text").css("color","blue");
+      });
+      $('#blue').mouseleave(function(){
+          $("#text").css("color", "black");
+      });
+
+  });
